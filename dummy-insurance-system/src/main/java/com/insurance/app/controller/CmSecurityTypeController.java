60a89651137fc0b1924f1998434bc47f7d24d002
InsuranceTeam <90261236+InsuranceTeam@users.noreply.github.com>
2021-10-03 20:38:58 +0900

    Add files via upload

--- dummy-insurance-system/src/main/java/com/insurance/app/controller/CmSecurityTypeController.java
+++ dummy-insurance-system/src/main/java/com/insurance/app/controller/CmSecurityTypeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/cm_security")
+@RequestMapping("/cm/security")
 public class CmSecurityTypeController {
 
   @GetMapping
--- dummy-insurance-system/src/main/java/com/insurance/app/controller/WcInputController.java
+++ dummy-insurance-system/src/main/java/com/insurance/app/controller/WcInputController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/wc_input")
+@RequestMapping("/wc/input")
 public class WcInputController {
 
   @GetMapping
