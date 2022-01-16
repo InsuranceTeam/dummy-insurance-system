c21f309fe8638ce4965d4c80949617ea9be48dcc
DN <>
2022-01-16 14:38:48 +0900

    update 20220116

--- dummy-insurance-system-sql/4.CREATE_wc_actuarial.sql
+++ dummy-insurance-system-sql/4.CREATE_wc_actuarial.sql
@@ -8,6 +8,6 @@ CREATE TABLE `insurance_db`.`wc_actuarial` (
   `apply_end_date` date NOT NULL COMMENT '適用終了日',
   `unit_price_premium` int NOT NULL COMMENT '掛金単価',
   `create_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `update_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   PRIMARY KEY (`security_type`,`entry_age`,`insured_person_sex`,`payment_method`,`payment_expiration_age`,`apply_start_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='数理テーブル（終身医療）'
